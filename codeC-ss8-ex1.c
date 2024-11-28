ce3a3e0635420f239594389ef1e2c806c1e8d35a
hieutitz <nt.hieu616@gmail.com>
2024-11-28 23:14:29 +0700

    Add files via upload

---
+++ codeC-ss8-ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = size - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
